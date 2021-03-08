de87288fbf1d7076c44e3f63b34c9e5428140593
Gianfranco Saenz Berrocal <gianfranco.saenz.berrocal@b5s201pc22.fib.upc.es>
2021-03-08 16:47:15 +0100

    nou fet

--- s1/s1h.s
+++ s1/s1h.s
@@ -1,24 +1,25 @@
 # PRACTICA 1 #######################
 
 	.data
-	cadena: .byte -1, -1, -1, -1, -1, -1
-	vec: .word 5, 6, 8, 9, 1
+cadena: .byte -1, -1, -1, -1, -1, -1
+vec: 	.word 5, 6, 8, 9, 1
 	.text
 	.globl main
 main:
 	li $s0, 0
-while:	li $t0, 5
+	
+while:	
+	li $t0, 5
 	bge $s0, $t0, fi
-	la $t0, vec
-	li $t1, 4
-	subu $t1, $t1, $s0
-	sll $t1, $t1, 2
-	addu $t0, $t0, $t1
-	lw $t0, 0($t0) # $t0 = vec[4-1]
-	addiu $t0, $t0, '0'
+	
 	la $t1, cadena
+	la $t2, vec
+	sll $t3, $s0, 2
+	subu $t2, $t2, $t3
+	lw $t3, 16($t2)
+	addiu $t3, $t3, 0x30
 	addu $t1, $t1, $s0
-	sb $t0, 0($t1)
+	sb $t3, 0($t1)
 	
 	addiu $s0, $s0, 1
 	b while
@@ -27,7 +28,7 @@ fi:
 	sb $zero, 5($t0)
 	
 	li $v0, 4
-	la $a0, cadena
+	move $a0, $t0
 	syscall
 	
 	jr $ra
--- s1/s2g.s
+++ s1/s2g.s
@@ -1,34 +1,28 @@
 # PRACTICA 1 #######################
 
 	.data
-	fib: .space 40
+fib: 	.space 40
 	.text
 	.globl main
 main:
 	
 	li $s0, 2
-	la $t0, fib
-	sw $zero, 0($t0)
-	li $t1, 1
-	sw $t1, 4($t0)
+	la $t1, fib
+	li $t2, 1
+	sw $zero, 0($t1)
+	sw $t2, 4($t1)
 while:
 	slti $t0, $s0, 10
 	beq $t0, $zero, fi
 	
-	la $t0, fib
-	addiu $t1, $s0, -1
-	sll $t1, $t1, 2
-	addu $t1, $t1, $t0
-	lw $t2, 0($t1)
-	# $t0 = fib
-	addiu $t1, $s0, -2
-	sll $t1, $t1, 2
-	addu $t1, $t1, $t0
-	lw $t3, 0($t1)
-	sll $t1, $s0, 2
-	addu $t1, $t0, $t1
-	addu $t4, $t2, $t3
-	sw $t4, 0($t1)
+	sll $t2, $s0, 2
+	addu $t0, $t1, $t2
+	lw $t2, -4($t0)
+
+	lw $t3, -8($t0)
+
+	addu $t2, $t2, $t3
+	sw $t2, 0($t0)
 	
 	addiu $s0, $s0, 1
 	b while
