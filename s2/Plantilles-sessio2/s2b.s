ca48e7aa5df2780a7e28a9214a45397910ab79d0
Gianfranco Saenz Berrocal <gianfranco.saenz.berrocal@b5s201pc22.fib.upc.es>
2021-03-22 17:00:18 +0100

    ACABAR

--- s2/Plantilles-sessio2/s2b.s
+++ s2/Plantilles-sessio2/s2b.s
@@ -6,7 +6,35 @@ num:	.byte '7'
 	.globl main
 main:
 
-# COPIA EL TEU CODI AQUI
+	la $t0, result
+	la $t1, num
+	lb $t1, 0($t1)
+
+if1:
+	li $t2, 'a'
+	blt $t1, $t2, if2
+	li $t2, 'z'
+	ble $t1, $t2, exe
+
+if2:	
+	li $t2, 'A'
+	blt $t1, $t2, else
+	li $t2, 'Z'
+	ble $t1, $t2, exe
+	
+else:
+	li $t2, '9'
+	bgt $t1, $t2, nope
+	li $t2, '0'
+	blt $t1, $t2, nope
+	subu $t1, $t1, $t2
+	b exe
+
+nope:
+	li $t1, -1
+	
+exe: 
+	sw $t1, 0($t0)
 
 	jr $ra
 
--- s2/Plantilles-sessio2/s2c.s
+++ s2/Plantilles-sessio2/s2c.s
@@ -38,11 +38,96 @@ nofares:
 
 
 moda:
-# ESCRIU AQUI EL TEU CODI
+	addiu $sp, $sp, -60 #guardem les variables locals i les que volem salvar
+	sw $a0, 48($sp) #@ de w
+	sw $a1, 52($sp)	#31
+	sw $ra, 56($sp)
+	
+	li $t0, 0 #k = 0
+	li $t1, 11 #k' = 10
+	#char max = '0' ---> fer un sw
+	move $t2, $sp #t2 = @sp (que no és el vector histo)
+	addiu $t2, $t2, 4 #ara sí és l'adreça del vector histo)
+for:
+	ble $t1, $t0, end
+	sw $zero, 0($t2)
+	addiu $t2, $t2, 4 #ens movem al histo (següent posició)
+	addiu $t1, $t1, -1 
+	b for
 
+end:
+	li $s3, '0'
+	move $s0, $a0 #guardem l'adreça de w
+	move $s1, $a1 #guardem 31
+	move $s2, $t0 #guardem $t0 = k
+
+for2:
+	bge $s2, $s1, end2
+	
+	addiu $a0, $t2, -40
+	addu $a1, $s0, $s2
+	
+	lb $a1, 0($a1)
+	
+	addiu $a1, $a1, -48
+	addiu $a2, $s3, -48
+	
+	jal update
+	
+	addiu $s3, $v0, '0'
+	addiu $s2, $s2, 1
+	
+	b for2
+	
+end2:
+
+	move $v0, $s3 #return de max que es troba a s3
+	lw $a0, 48($sp) 
+	lw $a1, 52($sp)	
+	lw $ra, 56($sp)
 
 
 update:
-# ESCRIU AQUI EL TEU CODI
+	
+	addiu $sp, $sp, -16
+	sw $s0, 0($sp) #@ de w
+	sw $s1, 4($sp)
+	sw $s2, 8($sp)
+	sw $ra, 12($sp)
+	
+	move $s0, $a0
+	move $s1, $a1 
+	move $s2, $a2
+	
+	jal nofares
+	
+	sll $t0, $s1, 2
+	addu $t0, $s0, $t0
+	lw $t1, 0($t0)
+	addiu $t1, $t1, 1
+	sw $t1, 0($t0)
+	
+	sll $t2, $s2, 2
+	addu $t2, $s0, $t2
+	lw $t2, 0($t2)
+	
+	ble $t1, $t2, else
+	move $v0, $s1
+	
+	b end3
+	
+else:
+	move $v0, $s2
+	
+end3:
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	lw $s2, 8($sp)
+	lw $s3, 12($sp)
+	
+	addiu $sp, $sp, 16
+	
+	jr $ra
+
 
 
