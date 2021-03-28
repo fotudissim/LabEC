f201911dd870c5875612d3b0d0a1f264b7172076
fotudissim <gsaenz.berrocal@gmail.com>
2021-03-29 00:48:47 +0200

    s2c.s versió final

--- s2/Plantilles-sessio2/s2c.s
+++ s2/Plantilles-sessio2/s2c.s
@@ -38,96 +38,112 @@ nofares:
 
 
 moda:
-	addiu $sp, $sp, -60 #guardem les variables locals i les que volem salvar
-	sw $a0, 48($sp) #@ de w
-	sw $a1, 52($sp)	#31
-	sw $ra, 56($sp)
-	
-	li $t0, 0 #k = 0
-	li $t1, 11 #k' = 10
-	#char max = '0' ---> fer un sw
-	move $t2, $sp #t2 = @sp (que no és el vector histo)
-	addiu $t2, $t2, 4 #ara sí és l'adreça del vector histo)
-for:
-	ble $t1, $t0, end
-	sw $zero, 0($t2)
-	addiu $t2, $t2, 4 #ens movem al histo (següent posició)
-	addiu $t1, $t1, -1 
-	b for
+    #a0 = &w; a1 = 31
+    addiu $sp, $sp, -44 #On 40 bytes son pel vector com en v.local i 4 son per guardar el ra de tornada
+    #int histo[10]: 0(sp), int k: s0, char max: s1;; ra: 40(sp), a1: no cal, a2: no cal
+    #sw $a0, 40($sp) no cal guardar 
+    #sw $a1, 44($sp) no cal guardar (2)
+    sw $ra, 40($sp)
+for1:
+    move $t1, $sp
+    addiu $t2, $t1, 40
+    bgeu $t1, $t2, end1
+loop1:
+    sw $zero, 0($t1)
+    addiu $t1, $t1, 4
+    bltu $t1, $t2, loop1
+end1:
+#final del primer for, on tots els elements de histo son equivalents a 0
+    
+    move $s0, $a0 #guardem &w en a0 (també anomenat: &vec, IMPORTANT: vec / w és char)
+    move $s1, $a1 #guardem 31 en a1
+    li $s2, '0' #guardem '0' en s2 ---> s2 és el nostre char max, llavors no cal guardar a memòria
+    li $s3, 0 #guardem 0 a s3 ----> s3 és el nostre int k, llavors no cal guardar a memòria
 
-end:
-	li $s3, '0'
-	move $s0, $a0 #guardem l'adreça de w
-	move $s1, $a1 #guardem 31
-	move $s2, $t0 #guardem $t0 = k
-
+#nou loop for on haurem d'entrar a una altre funció (8)
 for2:
-	bge $s2, $s1, end2
-	
-	addiu $a0, $t2, -40
-	addu $a1, $s0, $s2
-	
-	lb $a1, 0($a1)
-	
-	addiu $a1, $a1, -48
-	addiu $a2, $s3, -48
-	
-	jal update
-	
-	addiu $s3, $v0, '0'
-	addiu $s2, $s2, 1
-	
-	b for2
-	
-end2:
+    bgeu $s3, $s1, endfor
+loop2:
+    #preparem els paràmetres nous: a0 = &histo (fem un move $a0, $sp), 
+    move $a0, $sp
+    #w[k]-'0' --> (&vec + k) - '0'
+    addu $a1, $s0, $s3
+    lb $a1, 0($a1)
+    addiu $a1, $a1, -48
+    #max-'0'
+    addiu $a2, $s2, -48
+    
+    #crida a la funció update
+    jal update
+    #havent tornat de update, hem recuperat s0, s1, s2
+    addiu $s2, $v0, 48 #max = update + '0'
+    addiu $s3, $s3, 1 #k++
+    bltu $s3, $s1, loop2
 
-	move $v0, $s3 #return de max que es troba a s3
-	lw $a0, 48($sp) 
-	lw $a1, 52($sp)	
-	lw $ra, 56($sp)
+endfor:
+    #guardem max a v0 per fer el return (1)
+    move $v0, $s2
+    #load word de l'antic ra
+    lw $ra, 40($sp)
+    #destrucció del b.activació
+    addiu $sp, $sp, 44
+    jr $ra
 
 
 update:
-	
-	addiu $sp, $sp, -16
-	sw $s0, 0($sp) #@ de w
-	sw $s1, 4($sp)
-	sw $s2, 8($sp)
-	sw $ra, 12($sp)
-	
-	move $s0, $a0
-	move $s1, $a1 
-	move $s2, $a2
-	
-	jal nofares
-	
-	sll $t0, $s1, 2
-	addu $t0, $s0, $t0
-	lw $t1, 0($t0)
-	addiu $t1, $t1, 1
-	sw $t1, 0($t0)
-	
-	sll $t2, $s2, 2
-	addu $t2, $s0, $t2
-	lw $t2, 0($t2)
-	
-	ble $t1, $t2, else
-	move $v0, $s1
-	
-	b end3
-	
-else:
-	move $v0, $s2
-	
-end3:
-	lw $s0, 0($sp)
-	lw $s1, 4($sp)
-	lw $s2, 8($sp)
-	lw $s3, 12($sp)
-	
-	addiu $sp, $sp, 16
-	
-	jr $ra
+#en aquesta funció, tenim 3 paràmetres: a0 = &histo, a1 = char i, a2 = char imax
+#compte! el vector histo, o int h, es passa com a punter, és a dir, que tot canvi s'ha de guardar
+#guardar registres segurs i expandir el $sp
+    addiu $sp, $sp, -16 #perquè volem guardar a s0-s2 els tres paràmetres a0-a1 i el ra a main
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+    sw $ra, 12($sp)
 
+    #guardem ses registres: a0, a1, a2 a s0, s1, s2
+    move $s0, $a0 #histo[] en s0
+    move $s1, $a1 #i en s1
+    move $s2, $a2 #imax en s2
+
+    jal nofares
+
+    #h[i]++ ---> vector h (&h en s0 and char i en s1)
+    sll $t0, $s1, 2 #char i * 4 == int i
+    addu $t0, $s0, $t0 #&h + i --> t0
+    lw $t1, 0($t0) #t1 = h[i]
+    addiu $t1, $t1, 1 #contingut de h[i] + 1 guardat a t1
+    sw $t1, 0($t0) #h[i] = h[i] + 1 = t1 (es guarda al vector, i com es descriu al enunciat, es pasa per referència)
+    
+
+#preparar imax per el if(2)
+    sll $t2, $s2, 2 #char imax * 4 == int char imax
+    addu $t2, $s0, $t2 #&h + imax ---> t2
+    lw $t2, 0($t2) #t2 = h[imax]
+
+
+if:
+    bleu $t1, $t2, else #si t1 <= t2, llavors fem branch a endif //t1 = h[i]; t2 = h[imax]
+    move $v0, $s1
+    b endif #preparem el return
+else:
+    move $v0, $s2
+
+endif:
+    #recuperem els registres guardats
+    lw $s0, 0($sp)
+    lw $s1, 4($sp)
+    lw $s2, 8($sp)
+    lw $ra, 12($sp)
+    #tanquem el bloc d'activació
+    addiu $sp, $sp, 16
+    #tornem a moda
+    jr $ra
+
+
+
+
+
+
+	
 
 
